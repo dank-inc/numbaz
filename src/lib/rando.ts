9c30bfcd35b501e33eab47afb0ba5bf5ea51a4f0
elijah <elijahlucian@gmail.com>
2025-03-02 12:45:07 -0700

    0.8.0

--- src/lib/rando.ts
+++ src/lib/rando.ts
@@ -7,7 +7,8 @@ export const int = (min = 0, max = 42) => Math.floor(range(min, max))
 export const normal = (scale = 1, offset = 0) =>
   (num() - 0.5) * 2 * scale + offset
 
-export const item = <T>(arr: Array<T>): T => {
-  const i = Math.floor(num() * (arr.length - 1))
-  return arr[i]
-}
+export const item = <T>(arr: Array<T>): T => arr[int(0, arr.length - 1)]
+
+export const bool = () => Math.random() < 0.5
+
+export const pick = <T>(arr: Array<T>): T => arr[int(0, arr.length - 1)]
